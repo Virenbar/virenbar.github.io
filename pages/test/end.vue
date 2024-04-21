5aec46c5671cd981e9063b179526783d8be805d6
Virenbar <Virenbar@users.noreply.github.com>
2024-04-21 19:23:01 +0500

    Add test backgrounds

---
+++ pages/test/end.vue
@@ -0,0 +1,39 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: "end"
+  }
+});
+</script>
+<template>
+  <div>
+    The End background test
+  </div>
+</template>
+<style>
+/* Source: https://rebane2001.com/mapartcraft/supporters/ */
+.end {
+  background: url("~/assets/image/end/bg1.png"), url("~/assets/image/end/bg1.png"), url("~/assets/image/end/bg2.png");
+  /* background-position: left top, left top; */
+  /* background-repeat: repeat, repeat, repeat; */
+  background-size: 512px, 768px, 32px;
+  /* background-color: black; */
+  background-blend-mode: screen;
+  animation-name: the_end;
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  /* overflow: scroll;
+  overflow-x: hidden; */
+}
+
+@keyframes the_end {
+  0% {
+    background-position: -512px 0, -768px 0, -256px -128px;
+  }
+
+  100% {
+    background-position: 0 0, 0 0, 0 0;
+  }
+}
+</style>
---
+++ pages/test/space.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="space__stars">
+    <div class="space__twinkling">
+      <div class="container">
+        The Space background test
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.space__stars,
+.space__twinkling {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: block
+}
+
+.space__stars {
+  background: url("~/assets/image/space/stars.png")top center;
+  z-index: 0
+}
+
+.space__twinkling {
+  background: url("~/assets/image/space/twinkling.png");
+  animation: 10s twinkle infinite;
+  animation-direction: alternate-reverse;
+  animation-timing-function: linear
+}
+
+@keyframes twinkle {
+  0% {
+    background-position: 0
+  }
+
+  to {
+    background-position: -100px 0
+  }
+}
+</style>
